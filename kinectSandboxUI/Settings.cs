442894d43090b730a91be481e95a388d2b74b2de
Julien Vinel <jvinel@gmail.com>
2015-06-15 22:11:25 +0200

    Add new UI based on Wpf Extended Toolkit

---
+++ kinectSandboxUI/Settings.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kinectSandboxUI
+{
+    public class Settings
+    {
+        
+
+        [CategoryAttribute("General"),
+        DisplayName("Min. Depth"),
+        DescriptionAttribute("Minimum depth of data processed")]
+        public int MinDepth
+        {
+            get;
+            set;
+        }
+
+        [CategoryAttribute("General"),
+        DisplayName("Max. Depth"),
+        DescriptionAttribute("Maximum depth of data processed")]
+        public int MaxDepth
+        {
+            get;
+            set;
+        }
+
+        [CategoryAttribute("Filtering"),
+        DisplayName("Stabilization"),
+        DescriptionAttribute("Activate stabilization filter")]
+        public bool ActivateStabilization
+        {
+            get;
+            set;
+        }
+
+        [CategoryAttribute("Filtering"),
+        DisplayName("Gradient"),
+        DescriptionAttribute("Gradient bitmap used to colorize image")]
+        public string Gradient
+        {
+            get;
+            set;
+        }
+
+        [CategoryAttribute("Filtering"),
+        DisplayName("Isolines"),
+        DescriptionAttribute("Isoline step: 0 desactivate, maximum 100")]
+        public int isolines
+        {
+            get;
+            set;
+        }
+
+    }
+}
